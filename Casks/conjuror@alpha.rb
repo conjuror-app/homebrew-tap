67d28a3ff317986ac73eaf73890341269522d0d9
Tom Bell <tomb@tombell.dev>
2026-08-24 22:24:48 +0100

    Update Conjuror alpha to 1.0-alpha.11

--- Casks/conjuror@alpha.rb
+++ Casks/conjuror@alpha.rb
@@ -1,8 +1,8 @@
 cask 'conjuror@alpha' do
-  version '1.0-alpha.10'
-  sha256 '4fe4f717b6d1b69e1739e29537ff2654ef050ae3597038805c9a1b322e0302ee'
+  version '1.0-alpha.11'
+  sha256 '0e4f6f774363ca731e47fa0c8467e344922e75d49922e0b8e5398d8af2b09353'
 
-  url "https://github.com/conjuror-app/releases/releases/download/#{version}/conjuror-1.0-10.zip"
+  url "https://github.com/conjuror-app/releases/releases/download/#{version}/conjuror-1.0-11.zip"
   name 'Conjuror'
   desc 'A little magic for your Mac. An app launcher.'
   homepage 'https://github.com/conjuror-app/releases'
